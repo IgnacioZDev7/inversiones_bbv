e2d43519731473c742789500c9daaef720346793
IgnacioZDev7 <ignaciozarate.dev@gmail.com>
2026-04-05 23:54:29 -0400

    Primer commit, separación del sistema frontend y backend, instalación de dependencias en el backend, creación del frontend con react y tailwind, conexión a la base de datos supabase exitosa y primeras migraciones

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-purple-700 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind funcionando</h1>
+    </div>
+  );
+}
